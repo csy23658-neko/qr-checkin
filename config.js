4eb982e98072c325cef99359404cb312436de2cd
csy23658-neko <csy23658@gmail.com>
2026-08-01 23:42:44 +0800

    Configure staging API endpoint

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const CONFIG = {
   GOOGLE_CLIENT_ID: '234893329401-s9cnhole5d3q885do2b5349btlifk46r.apps.googleusercontent.com',
 
   // Empty when the API Worker is routed beneath this same domain.
-  API_BASE_URL: '',
+  API_BASE_URL: 'https://qr-checkin-api-staging.csy23658.workers.dev',
 };
