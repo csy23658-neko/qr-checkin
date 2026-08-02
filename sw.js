9f1eee17c457a246c50f22c722136f4a1dca6675
csy23658-neko <csy23658@gmail.com>
2026-08-02 11:48:43 +0800

    Refresh cache after activity setup changes

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Lets the app shell load with poor/no connectivity (data still needs network). */
 'use strict';
 
-const CACHE = 'qr-checkin-v2';
+const CACHE = 'qr-checkin-v3';
 const ASSETS = [
   './',
   './index.html',
